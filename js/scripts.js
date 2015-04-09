51bb598e84b5ec7050e13341fba28ed5bce67a9b
BJack <brianjacklinca@gmail.com>
2015-04-09 09:24:47 -0400

    prep for send

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,6 @@
 
 window.onload = function() {
   var $ = function (selector){return document.querySelector(selector);};  // this is not jQuery
-
-/* ############################################################## 
-                      Navigation
-   ############################################################## */
-  function toggleMenu(){
-    $("#main-nav").classList.toggle("open");
-    $(".overlay").classList.toggle("open");
-    $(".trigger").classList.toggle("open");
-    $("#body").classList.toggle("open");
-  }
-  
-  //behaviour if you click the menu button
-  $(".trigger").onclick = toggleMenu;
-  
-  //behaviour if you click a menu item
-  $("#main-nav").getElementsByTagName("li").onclick = toggleMenu;
-  
-  //behaviour if you click the overlay
-  $(".overlay").onclick = toggleMenu;
-
-
-/* ############################################################## 
-                      ASSIGNMENT #1 START
-   ############################################################## */
-   
   var allItems = document.querySelectorAll(".item");  
   var targets = $("#accordion").getElementsByTagName("a");
 
@@ -37,7 +12,6 @@ window.onload = function() {
 
   hideAll(); // ensure that even all accordions are closed on load. Remove this if you want an item open by default
 
-
   /* accordion */
   function accordion(){
 
@@ -47,7 +21,7 @@ window.onload = function() {
 
         event.preventDefault(); // stops first click from trying to fire anchor href.  
 
-        if(this.parentNode.classList.contains("open") == false){
+        if(this.parentNode.classList.contains("open") === false){
           hideAll();
           this.parentNode.classList.add("open");
         }else{
